c2760be86bc663255808028991cc752c94e10619
Torben <torbinsky@gmail.com>
2020-12-01 12:33:40 -0800

    implement part 2 solution

--- day1/cmd/main.go
+++ day1/cmd/main.go
@@ -44,16 +44,46 @@ func main() {
 		log.Fatal(err)
 	}
 
+	sort.Ints(viableValues) // sort required for solution algorithms
+
 	// Find the pair that sums to 2020
-	val1, val2, err := solve(viableValues)
+	val1, val2, err := solvePart1(viableValues)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Solution: %d * %d = %d\n", val1, val2, val1*val2)
+	fmt.Printf("Part 1 Solution: %d * %d = %d\n", val1, val2, val1*val2)
+
+	val1, val2, val3, err := solvePart2(viableValues)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Part 1 Solution: %d * %d * %d = %d\n", val1, val2, val3, val1*val2*val3)
 }
 
-func solve(values []int) (int, int, error) {
-	sort.Ints(values)
+// solvePart2 finds 3 numbers in a sorted list that sum to 2020
+// NOTE: expects an already sorted integer list to work properly
+func solvePart2(values []int) (int, int, int, error) {
+	var sum int
+	for idx, val1 := range values {
+		for i := idx; i < len(values); i++ {
+			for j := i; j < len(values); j++ {
+				sum = val1 + values[i] + values[j]
+				if sum == 2020 {
+					return val1, values[i], values[j], nil
+				}
+				if sum > 2020 {
+					break
+				}
+			}
+		}
+	}
+
+	return 0, 0, 0, fmt.Errorf("no solution")
+}
+
+// solvePart1 finds 2 numbers in a sorted list that sum to 2020
+// NOTE: expects an already sorted integer list to work properly
+func solvePart1(values []int) (int, int, error) {
 	for idx, val1 := range values {
 		for i := idx; i < len(values); i++ {
 			if val1+values[i] == 2020 {
