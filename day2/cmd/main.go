caadcc0f509d319503d30659d2db200b3c1c0b64
Torben <torbinsky@gmail.com>
2020-12-01 21:53:14 -0800

    implement part 2

--- day2/cmd/main.go
+++ day2/cmd/main.go
@@ -28,15 +28,20 @@ func main() {
 	// Scan in numeric data
 
 	scanner := bufio.NewScanner(file)
-	validCount := 0
+	validPart1Count := 0
+	validPart2Count := 0
 	for scanner.Scan() {
 		policy, password, err := processLine(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if validate(password, policy) {
-			validCount++
+		if validatePart1(password, policy) {
+			validPart1Count++
+		}
+
+		if validatePart2(password, policy) {
+			validPart2Count++
 		}
 	}
 
@@ -44,14 +49,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Found %d valid passwords\n", validCount)
-
+	fmt.Printf("Part1 - found %d valid passwords\n", validPart1Count)
+	fmt.Printf("Part2 - found %d valid passwords\n", validPart2Count)
 }
 
+// encodes the information in the toboggan password policy database
+//
+// Example:
+// 1-3 a: abcde
+// v1 = 1
+// v2 = 3
+// char = "a"
 type passwordPolicy struct {
-	min  int    // min occurrences of char
-	max  int    // max occurrences of char
-	char string // the char that must occur
+	v1   int  // first numerical value of password policy
+	v2   int  // second numerical value of password policy
+	char rune // the char that must occur
 }
 
 func processLine(text string) (*passwordPolicy, string, error) {
@@ -60,26 +72,33 @@ func processLine(text string) (*passwordPolicy, string, error) {
 		return nil, "", fmt.Errorf("line does not match expected format")
 	}
 
-	min, err := strconv.Atoi(results[1])
+	v1, err := strconv.Atoi(results[1])
 	if err != nil {
 		return nil, "", err
 	}
 
-	max, err := strconv.Atoi(results[2])
+	v2, err := strconv.Atoi(results[2])
 	if err != nil {
 		return nil, "", err
 	}
 
 	return &passwordPolicy{
-		min:  min,
-		max:  max,
-		char: results[3],
+		v1:   v1,
+		v2:   v2,
+		char: rune(results[3][0]),
 	}, results[4], nil
 }
 
-func validate(password string, policy *passwordPolicy) bool {
-	count := strings.Count(password, policy.char)
-	if count <= policy.max && count >= policy.min {
+func validatePart2(password string, policy *passwordPolicy) bool {
+	c1 := []rune(password)[policy.v1-1]
+	c2 := []rune(password)[policy.v2-1]
+
+	return c1 == policy.char && c2 != policy.char || c1 != policy.char && c2 == policy.char
+}
+
+func validatePart1(password string, policy *passwordPolicy) bool {
+	count := strings.Count(password, string(policy.char))
+	if count <= policy.v2 && count >= policy.v1 {
 		return true
 	}
 
