c6565fbfde5e66772ac94ab0255fdab84aa43989
Torben <torbinsky@gmail.com>
2020-12-03 08:10:25 -0800

    implement day3 - toboggan map slope

---
+++ day3/cmd/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+// The rune that indicates a tree tile on the map
+const tree = '#'
+
+// Buffer to store each line of a file
+var fileLines []string
+
+// loads an entire file into our buffer of lines and returns the map dimensions (width,height)
+func loadFile(path string) (int, int) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fileLines = append(fileLines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return len(fileLines[0]), len(fileLines)
+}
+
+// gets the map tile at the specified coordinate
+func getMapTile(x, y int) (rune, error) {
+	// Validate y coordinate is in map bounds
+	if y < 0 || y > len(fileLines) {
+		return 0, fmt.Errorf("y coord outside of map bounds")
+	}
+
+	// Validate x coordinate is in map bounds
+	if x < 0 || x > len(fileLines[y]) {
+		return 0, fmt.Errorf("x coord is outside of map bounds")
+	}
+
+	// y is the line num, x is the rune
+	return rune(fileLines[y][x]), nil
+}
+
+func main() {
+
+	// Validate program usage
+	if len(os.Args) != 2 {
+		log.Fatal("This command accepts only one argument: the path to the input file")
+	}
+	width, height := loadFile(os.Args[1])
+
+	// Part 1
+
+	fmt.Println("Part One")
+	fmt.Printf("Total trees: %d\n", traverseSlope(3, 1, width, height))
+	fmt.Println()
+
+	// Part 2
+
+	fmt.Println("Part Two")
+	total := 1
+	for _, val := range []struct {
+		dx int
+		dy int
+	}{
+		{dx: 1, dy: 1},
+		{dx: 3, dy: 1},
+		{dx: 5, dy: 1},
+		{dx: 7, dy: 1},
+		{dx: 1, dy: 2},
+	} {
+		next := traverseSlope(val.dx, val.dy, width, height)
+		fmt.Printf("- Right %d, down %d = %d\n", val.dx, val.dy, next)
+		total *= next
+	}
+
+	fmt.Printf("Total trees: %d\n", total)
+}
+
+// Fully traverses a "slope" across a map, counting the number of trees that are encountered
+//
+// A "slope" is defined by a horizontal (dx) and vertical (dy) distance that you move
+//
+// For example, dx=3,dy=1 means you move 3 tiles to the right, and 1 down (starting from the top left 0,0)
+//
+// "width" and "height" are the bounds of the map
+//
+// (0,0) is the top left, (width-1,height-1) is the bottom right
+func traverseSlope(dx, dy, width, height int) int {
+	treesEncountered := 0
+
+	right := 0
+	for down := dy; down < height; down += dy {
+		right = (right + dx) % width // the slope wraps around horizontally (this had me stuck for a while!)
+		tile, err := getMapTile(right, down)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if tile == tree {
+			treesEncountered++
+		}
+	}
+
+	return treesEncountered
+}
