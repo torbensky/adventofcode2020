38ee01ffb3b33be411855363f70b9de8aa3d4ae5
Torben <torbinsky@gmail.com>
2020-12-06 17:10:42 -0800

    add cmd to run all days' challenges

---
+++ cmd/all.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/torbensky/adventofcode2020/common"
+)
+
+func main() {
+	baseDir := common.GetInputFilePath()
+	files, err := ioutil.ReadDir(baseDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() && strings.HasPrefix(f.Name(), "day") {
+
+			fmt.Println("==========================================================")
+			fmt.Printf("= Running %s...\n", f.Name())
+			fmt.Println("==========================================================")
+			fmt.Println()
+
+			mainFile := filepath.Join(baseDir, f.Name(), "cmd", "main.go")
+			inputFile := filepath.Join(baseDir, f.Name(), "input.txt")
+			cmd := exec.Command("go", "run", mainFile, inputFile)
+
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			// Execute the command
+			if err := cmd.Run(); err != nil {
+				log.Panic(err)
+			}
+		}
+	}
+}
