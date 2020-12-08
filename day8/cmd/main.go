9fa08a79a96df9d51ca25f54aac5c2cc236023b9
Torben <torbinsky@gmail.com>
2020-12-07 22:01:42 -0800

    refactor a few things for clarity and simplify

--- day8/cmd/main.go
+++ day8/cmd/main.go
@@ -3,23 +3,26 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"strconv"
 	"strings"
 
 	"github.com/torbensky/adventofcode2020/common"
 )
 
+type operator string
+
 // types of operations
 const (
-	nopOp = "nop"
-	accOp = "acc"
-	jmpOp = "jmp"
+	nopOp = operator("nop")
+	accOp = operator("acc")
+	jmpOp = operator("jmp")
 )
 
 // Represents an instruction
 type instruction struct {
-	op  string // the type of operation
-	arg int    // the argument for the operation
+	op  operator // the type of operation
+	arg int      // the argument for the operation
 }
 
 func main() {
@@ -27,22 +30,23 @@ func main() {
 
 	_, acc := executeProgram(program)
 	fmt.Printf("Part 1 - Result %d\n\n", acc)
-	fmt.Printf("Part 2 - Total %d\n", fixProgram(program))
+	fmt.Printf("Part 2 - Result %d\n", fixProgram(program))
 }
 
+// Fixes the program according to Part 2
 func fixProgram(instructions []*instruction) int {
 	for i, inst := range instructions {
-		var completes bool
-		var acc int
+		var new, old operator
 		switch inst.op {
 		case jmpOp:
-			completes, acc = testFixedProgram(instructions, i, nopOp, jmpOp)
+			new, old = nopOp, jmpOp
 		case nopOp:
-			completes, acc = testFixedProgram(instructions, i, jmpOp, nopOp)
+			new, old = jmpOp, nopOp
 		default:
 			continue
 		}
 
+		completes, acc := executePatch(instructions, i, new, old)
 		if completes {
 			return acc
 		}
@@ -51,15 +55,16 @@ func fixProgram(instructions []*instruction) int {
 	return -1
 }
 
-func testFixedProgram(instructions []*instruction, fixedIdx int, newOp, oldOp string) (bool, int) {
-	instructions[fixedIdx].op = newOp
+// Patches the program and executes that, returning the result
+func executePatch(instructions []*instruction, patchIdx int, newOp, oldOp operator) (bool, int) {
+	instructions[patchIdx].op = newOp
 	looped, acc := executeProgram(instructions)
-	instructions[fixedIdx].op = oldOp
+	instructions[patchIdx].op = oldOp
 	return looped, acc
 }
 
 // executes a program, halting if an infinite loop is detected
-// returns true/false depending on loop and the value left in the accumulator
+// returns true/false depending on whether a loop was found and the value left in the accumulator
 func executeProgram(instructions []*instruction) (bool, int) {
 	acc := 0
 	i := 0
@@ -72,40 +77,45 @@ func executeProgram(instructions []*instruction) (bool, int) {
 		}
 		executed[i] = struct{}{} // remember we executed this line
 
-		// Execute the instructin
+		// Execute the instruction
 		switch instructions[i].op {
 		case jmpOp:
 			i += instructions[i].arg
-			continue
 		case accOp:
 			acc += instructions[i].arg
-			fallthrough //
-		default:
-			// no-op
 			i++
+		case nopOp:
+			i++
+		default:
+			log.Fatalf("unknown instruction %s encountered on line %d", instructions[i].op, i)
 		}
 	}
 
 	return true, acc
 }
 
+// Loads a program from some data stream
 func loadProgram(reader io.Reader) []*instruction {
+
 	var instructions []*instruction
+
 	parseLine := func(line string) {
+
 		fields := strings.Fields(line)
 		instruction := &instruction{
-			op: fields[0],
+			// NOTE: in the real world, probably should validate this input. But meh for this :P
+			op: operator(fields[0]),
 		}
 
+		// parse out the argument
 		numStr := fields[1]
-		if numStr[0] == '+' {
-			numStr = numStr[1:]
-		}
 		val, err := strconv.Atoi(numStr)
 		common.MustNotError(err)
 		instruction.arg = val
+
 		instructions = append(instructions, instruction)
 	}
 	common.ScanLines(reader, parseLine)
+
 	return instructions
 }
