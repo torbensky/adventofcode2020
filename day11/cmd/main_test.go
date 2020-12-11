e474abbd56ad2a00145a80875c4a1935ded4178c
Torben Werner <torbensky@users.noreply.github.com>
2020-12-10 22:29:42 -0800

    first day 11 solution (with a bit of name cleanup)

---
+++ day11/cmd/main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+const part1Answer = 37
+const part2Answer = 26
+
+func TestPart1(t *testing.T) {
+	t.Parallel()
+	reader := bufio.NewReader(openTestInput(t))
+	got := part1(reader)
+	want := part1Answer
+	if want != got {
+		t.Errorf("expected %d got %d\n", want, got)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	t.Parallel()
+	reader := bufio.NewReader(openTestInput(t))
+	got := part2(reader)
+	want := part2Answer
+	if want != got {
+		t.Errorf("expected %d got %d\n", want, got)
+	}
+}
+
+func openTestInput(t *testing.T) *os.File {
+	file, err := os.Open("../test-input.txt")
+	if err != nil {
+		t.Fatalf("unable to open test data: %v\n", err)
+	}
+
+	return file
+}
